a982005300fa3bf8ea5925bbb5be037858e34137
arunt_000 <arunt_000@SCHOOLSPEAK30>
2018-03-21 16:11:01 +0530

    Demo Tads Modification

--- CommunityBrands.Demo.Tads/Controllers/TadsApiController.cs
+++ CommunityBrands.Demo.Tads/Controllers/TadsApiController.cs
@@ -8,15 +8,17 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Web.Script.Serialization;
 using System.Collections.Generic;
-using CB.IntegrationService.ApiModels;
-using CB.IntegrationService.ApiClient.Client;
+using CB.IntegrationService.ApiClient;
 using System.Net;
-using CB.IntegrationService.ApiClient.Api;
 using CommunityBrands.Demo.Tads.Models;
 using CommunityBrands.Demo.Tads.Utils;
 using CB.IntegrationService.StandardDataSet.Models;
 using EducationBrands.Demo.Tads.ServiceClasses;
 using System.Text;
+using CB.IntegrationService.ApiClient.Model;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
 namespace CommunityBrands.Demo.Controllers
 {
     [RoutePrefix("TadsApi")]
@@ -41,25 +43,19 @@ namespace CommunityBrands.Demo.Controllers
      //   [Authorize]
         [Route("Notification")]
         [HttpPost]
-        public IHttpActionResult Notification([FromBody]ProductNotificationRequest productNotificationRequest)
+        public HttpResponseMessage Notification([FromBody]CBISMessage cbisMessage)
         {
-            RequestTimeStamp = DateTime.Now;
-            int statusCode = 0;
-
             try
             {
-                ProcessRequest(productNotificationRequest);
-                statusCode = (int)HttpStatusCode.OK;
+               return ProcessRequest(cbisMessage);
+
             }
             catch (Exception ex)
             {
-                statusCode = (int)HttpStatusCode.InternalServerError;
-                return InternalServerError(ex);
+                //log error
             }
 
-            //Acknowlege the hub, whether the requset is processed successfully or not
-            AcknowledgeEbis(productNotificationRequest.EventToken, statusCode);
-            return Ok("Processed successfully");
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
         }
 
         /// <summary>
@@ -67,80 +63,62 @@ namespace CommunityBrands.Demo.Controllers
         /// </summary>
         /// <param name="ebEventPayload"></param>
         [NonAction]
-        public static void ProcessRequest(ProductNotificationRequest ebEventPayload)
+        public static HttpResponseMessage ProcessRequest(CBISMessage cbisMessage)
         {
-            if (ebEventPayload == null)
-                return;
+            if (cbisMessage == null)
+                return null;
 
-            //Deserilize json string as class object
-            System.Web.Script.Serialization.JavaScriptSerializer serializer = new JavaScriptSerializer();
-            MembersCollection deserilized = serializer.Deserialize<MembersCollection>(ebEventPayload.Payload);
-
-            string ebInstitutionId = ebEventPayload.EbInstitutionId;
-            string InstitutionName = ebEventPayload.InstitutionName;
             List<Applicant> applicants = new List<Applicant>();
-
-            foreach (Member mem in deserilized.Members)
+            List<CBISResult> lstCBISResult = new List<CBISResult>();
+            foreach (JObject jObject in cbisMessage.Data.Children<JObject>())
             {
-                Applicant applicant = new Applicant();
-                applicant.SchoolName = InstitutionName;
-                applicant.FirstName = mem.FirstName;
-                applicant.LastName = mem.LastName;
-                applicant.MiddleName = mem.MiddleName;
-                applicant.Gender = mem.Gender;
-                applicant.DOB = mem.BirthDate.ToString();
-
-                if (mem.Households != null && mem.Households.Count > 0)
+                Member mem = jObject.ToObject<Member>();
+                if (mem != null)
                 {
-                    Household houseHold = mem.Households[0];
-                    applicant.HouseholdName = houseHold.Name;
-                    if (houseHold.Phones != null && houseHold.Phones.Count > 0)
-                        applicant.HouseholdPhone = houseHold.Phones[0].Number;
-                    if (houseHold.EmailAddresses != null && houseHold.EmailAddresses.Count > 0)
-                        applicant.HouseholdEmail = houseHold.EmailAddresses[0].EmailId;
-                }
+                    Applicant applicant = new Applicant();
+                    applicant.FirstName = mem.FirstName;
+                    applicant.LastName = mem.LastName;
+                    applicant.MiddleName = mem.MiddleName;
+                    applicant.Gender = mem.Gender;
+                    applicant.DOB = mem.BirthDate.ToString();
 
-                if (mem.Grade != null)
-                    applicant.Grade = mem.Grade.Name;
-                applicants.Add(applicant);
+                    if (mem.Households != null && mem.Households.Count > 0)
+                    {
+                        Household houseHold = mem.Households[0];
+                        applicant.HouseholdName = houseHold.Name;
+                        if (houseHold.Phones != null && houseHold.Phones.Count > 0)
+                            applicant.HouseholdPhone = houseHold.Phones[0].Number;
+                        if (houseHold.EmailAddresses != null && houseHold.EmailAddresses.Count > 0)
+                            applicant.HouseholdEmail = houseHold.EmailAddresses[0].EmailId;
+                    }
+
+                    if (mem.Grade != null)
+                        applicant.Grade = mem.Grade.Name;
+                    applicants.Add(applicant);
+
+                    lstCBISResult.Add(new CBISResult
+                    {
+                        Id = "0",
+                        ResultType = "SUCCESS"
+                    });
+                }
             }
 
             DBHelper.InsertApplicantstoDB(applicants);
-        }
 
-        /// <summary>
-        /// Send acknowledge information to EBISs
-        /// </summary>
-        /// <param name="statusCode"></param>
-        /// <param name="statusMessage"></param>
-        private void AcknowledgeEbis(string eventToken, int statusCode)
-        {
-            NotificationAcknowledgeRequest notificationAckRequest = new NotificationAcknowledgeRequest()
+            CBISMessage cbisMessage_Response = new CBISMessage
             {
-                AcknowledgeTimestamp = DateTime.Now.ToString(),
-                RequestTimestamp = RequestTimeStamp.ToString(),
-                EventToken = eventToken,
-                StatusCode = statusCode.ToString(),
-                StatusMessage = statusCode == (int)HttpStatusCode.OK ? "Processed successfully" : "Operation failed"
+                CbInstitutionId = cbisMessage.CbInstitutionId,
+                EventName = cbisMessage.EventName,
+                MessageId = cbisMessage.MessageId,
+                MessageType = "NotificationResponse",
+                Model = "CBISResult",
+                Data = JContainer.FromObject(lstCBISResult)
             };
 
-            //Constructing header for the acknowledge request
-            Dictionary<string, string> dicHeaders = new Dictionary<string, string>();
-            //Product Id , Id for Tads in CBIS database
-            //pwd : Credentials for communication with CBIS
-            string prodId = "2";
-            string pwd = "xxxyyyzzz";
-            string basicCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(prodId + ":" + pwd));
-            dicHeaders.Add("Authorization", "Basic " + basicCredentials);
-            dicHeaders.Add("ProductId", "2");
-            dicHeaders.Add("ProductName", "Tads");
-            dicHeaders.Add("ProductSecret", "xxxyyyzzz");
-
-            Configuration conf = new Configuration();
-            conf.DefaultHeader = dicHeaders;
-            //Send acknowledge notification to EBIS
-            DataExchangeApi instance = new DataExchangeApi(conf);
-            instance.NotificationAcknowledge(notificationAckRequest);
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(new JavaScriptSerializer().Serialize(cbisMessage_Response));
+            return response;
         }
     }
 }
