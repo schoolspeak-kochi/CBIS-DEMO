594de73aec5111109d80e6fef313329a0abafac3
arunt_000 <arunt_000@SCHOOLSPEAK30>
2018-03-26 12:19:32 +0530

    Tads Controoler - enhancements

--- CommunityBrands.Demo.Tads/Controllers/TadsApiController.cs
+++ CommunityBrands.Demo.Tads/Controllers/TadsApiController.cs
@@ -39,99 +39,89 @@ namespace CommunityBrands.Demo.Controllers
             return Ok("PongPost TadsDemo @ " + DateTime.Now.ToString("dd-MM-yyyy hh:mm:ss tt"));
         }
 
-       // [TadsAuthorization]
-     //   [Authorize]
+        // [TadsAuthorization]
+        //   [Authorize]
         [Route("Notification")]
         [HttpPost]
         public HttpResponseMessage Notification([FromBody]CBISMessage cbisMessage)
         {
+            if (cbisMessage == null || cbisMessage.Data == null)
+            {
+                throw new ArgumentNullException();
+            }
+
             try
             {
-               return ProcessRequest(cbisMessage);
+                JToken token = cbisMessage.Data;
+                List<Person> lstPerson = new List<Person>();
+
+                if (token is JArray)
+                {
+                    lstPerson = token.ToObject<List<Person>>();
+                }
+                else if (token is JObject)
+                {
+                    lstPerson.Add(token.ToObject<Person>());
+                }
+
+                List<Applicant> applicants = new List<Applicant>();
+                List<CBISResult> lstCBISResult = new List<CBISResult>();
+
+                foreach (Person mem in lstPerson)
+                {
+                    if (mem != null)
+                    {
+                        Applicant applicant = new Applicant();
+                        applicant.FirstName = mem.FirstName;
+                        applicant.LastName = mem.LastName;
+                        applicant.MiddleName = mem.MiddleName;
+                        applicant.Gender = mem.Gender;
+                        applicant.DOB = mem.BirthDate.ToString();
+
+                        if (mem.Households != null && mem.Households.Count > 0)
+                        {
+                            Household houseHold = mem.Households[0];
+                            applicant.HouseholdName = houseHold.Name;
+                            if (houseHold.Phones != null && houseHold.Phones.Count > 0)
+                                applicant.HouseholdPhone = houseHold.Phones[0].Number;
+                            if (houseHold.EmailAddresses != null && houseHold.EmailAddresses.Count > 0)
+                                applicant.HouseholdEmail = houseHold.EmailAddresses[0].EmailId;
+                        }
+
+                        if (mem.Grade != null)
+                            applicant.Grade = mem.Grade.Name;
+                        applicants.Add(applicant);
+
+                        lstCBISResult.Add(new CBISResult
+                        {
+                            Id = "0",
+                            ResultType = "SUCCESS"
+                        });
+                    }
+                }
+
+                DBHelper.InsertApplicantstoDB(applicants);
+
+                CBISMessage cbisMessage_Response = new CBISMessage
+                {
+                    CbInstitutionId = cbisMessage.CbInstitutionId,
+                    EventName = cbisMessage.EventName,
+                    MessageId = cbisMessage.MessageId,
+                    MessageType = "NotificationResponse",
+                    Model = "CBISResult",
+                    Origin = "Tads",
+                    Data = JContainer.FromObject(lstCBISResult)
+                };
+
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = new StringContent(new JavaScriptSerializer().Serialize(cbisMessage_Response));
+                return response;
             }
             catch (Exception ex)
             {
-                //log error
                 return new HttpResponseMessage(HttpStatusCode.BadRequest);
-            }      
-        }
-
-        /// <summary>
-        /// Processing the request
-        /// </summary>
-        /// <param name="ebEventPayload"></param>
-        [NonAction]
-        public static HttpResponseMessage ProcessRequest(CBISMessage cbisMessage)
-        {
-            if (cbisMessage == null || cbisMessage.Data == null)
-            {
-                throw new Exception();
             }
 
-            JToken token = cbisMessage.Data;
-            List<Person> lstPerson = new List<Person>();
-
-            if (token is JArray)
-            {
-                lstPerson = token.ToObject<List<Person>>();
-            }
-            else if (token is JObject)
-            {
-                lstPerson.Add(token.ToObject<Person>());
-            }
-
-            List<Applicant> applicants = new List<Applicant>();
-            List<CBISResult> lstCBISResult = new List<CBISResult>();
-
-            foreach (Person mem in lstPerson)
-            {
-                if (mem != null)
-                {
-                    Applicant applicant = new Applicant();
-                    applicant.FirstName = mem.FirstName;
-                    applicant.LastName = mem.LastName;
-                    applicant.MiddleName = mem.MiddleName;
-                    applicant.Gender = mem.Gender;
-                    applicant.DOB = mem.BirthDate.ToString();
-
-                    if (mem.Households != null && mem.Households.Count > 0)
-                    {
-                        Household houseHold = mem.Households[0];
-                        applicant.HouseholdName = houseHold.Name;
-                        if (houseHold.Phones != null && houseHold.Phones.Count > 0)
-                            applicant.HouseholdPhone = houseHold.Phones[0].Number;
-                        if (houseHold.EmailAddresses != null && houseHold.EmailAddresses.Count > 0)
-                            applicant.HouseholdEmail = houseHold.EmailAddresses[0].EmailId;
-                    }
-
-                    if (mem.Grade != null)
-                        applicant.Grade = mem.Grade.Name;
-                    applicants.Add(applicant);
-
-                    lstCBISResult.Add(new CBISResult
-                    {
-                        Id = "0",
-                        ResultType = "SUCCESS"
-                    });
-                }
-            }
-
-            DBHelper.InsertApplicantstoDB(applicants);
-
-            CBISMessage cbisMessage_Response = new CBISMessage
-            {
-                CbInstitutionId = cbisMessage.CbInstitutionId,
-                EventName = cbisMessage.EventName,
-                MessageId = cbisMessage.MessageId,
-                MessageType = "NotificationResponse",
-                Model = "CBISResult",
-                Origin = "Tads",
-                Data = JContainer.FromObject(lstCBISResult)
-            };
-
-            var response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StringContent(new JavaScriptSerializer().Serialize(cbisMessage_Response));
-            return response;
         }
     }
 }
